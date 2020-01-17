92cc5e271dbaea5268de4801f784dfdc91a7f2e2
schultka <mail@tobias-schultka.com>
2020-01-18 00:05:52 +0100

    added electron

---
+++ public/Main.js
@@ -0,0 +1,9 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+  // win.loadFile("public/index.html");
+  win.loadURL("http://localhost:3000/");
+}
+
+app.on("ready", createWindow);
