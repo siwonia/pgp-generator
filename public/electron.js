d2de32882529c34cbce87d4dc200b77153ca8c8c
schultka <mail@tobias-schultka.com>
2020-01-18 00:39:28 +0100

    added build script

--- public/electron.js
+++ public/electron.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 });
   // win.loadFile("public/index.html");
-  win.loadURL("http://localhost:3000/");
+  // win.loadURL("http://localhost:3000/");
+  win.loadURL(`file://${path.join(__dirname, "../build/index.html")}`);
 }
 
 app.on("ready", createWindow);
