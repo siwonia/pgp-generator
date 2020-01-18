da650d42b0369e6b50629b2f66ef18b9b3db43f0
schultka <mail@tobias-schultka.com>
2020-01-18 01:30:05 +0100

    added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        TEST
-      </header>
-    </div>
+    <Main>
+      <Box />
+      <Box />
+    </Main>
   );
 }
 
 export default App;
+
+const Main = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+`;
+
+const Box = styled.div`
+  background-color: #383d47;
+  display: flex;
+  flex: 1;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  :last-child {
+    margin-right: 10px;
+  }
+`;
